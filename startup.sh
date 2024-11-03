09bbabafb4414eb0ab3e0efd1e3def4dba378989
Nikita Goncharov <nikitagon5@gmail.com>
2024-11-03 18:29:56 -0500

    changed startup script minorly

--- startup.sh
+++ startup.sh
@@ -9,6 +9,21 @@ command_exists () {
 echo "Installing required packages..."
 npm install
 
+# Create a .env file with generated keys
+cat <<EOL > .env
+# Environment variables declared in this file are automatically made available to Prisma.
+# See the documentation for more detail: https://pris.ly/d/prisma-schema#accessing-environment-variables-from-the-schema
+
+# Prisma supports the native connection string format for PostgreSQL, MySQL, SQLite, SQL Server, MongoDB and CockroachDB.
+# See the documentation for all the connection string options: https://pris.ly/d/connection-strings
+
+DATABASE_URL="file:./dev.db"
+ACCESS_TOKEN_SECRET=$(openssl rand -hex 32)
+REFRESH_TOKEN_SECRET=$(openssl rand -hex 32)
+EOL
+
+echo ".env file created with generated secrets."
+
 # Run database migrations
 echo "Running database migrations..."
 npx prisma migrate deploy
