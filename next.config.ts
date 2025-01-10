4116ae142490ab00522e1c598bd21dcf82e81cbc
Nikita Goncharov <nikitagon5@gmail.com>
2025-01-10 14:19:25 -0500

    other changes to next config

--- next.config.ts
+++ next.config.ts
@@ -5,22 +5,20 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        module: false,
-      };
-
       config.plugins.push(
         new MonacoWebpackPlugin({
           languages: ['javascript', 'typescript', 'python', 'cpp', 'java', 'c'],
-          filename: 'static/[name].worker.js',
+          filename: 'static/[name].worker.js'
         })
       );
     }
+
     return config;
   },
-  transpilePackages: ['monaco-editor']
+  transpilePackages: ['monaco-editor'],
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
