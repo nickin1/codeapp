df48eef0399b11152f4a3ab53bd63a64f1ca3091
Nikita Goncharov <nikitagon5@gmail.com>
2025-01-10 15:23:38 -0500

    added head

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className="bg-background min-h-screen">
         <SearchProvider>
           <Providers>
