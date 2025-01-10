d56008db4e4b0b819eb9a12eb408e8a20c9a8315
Nikita Goncharov <nikitagon5@gmail.com>
2025-01-10 14:09:28 -0500

    fixed auth file problems

--- pages/api/templates.ts
+++ pages/api/templates.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/auth.config";
 import { prisma } from '@/lib/prisma';
 
 interface CreateTemplateBody {
