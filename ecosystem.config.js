63d7947b10449e8d7647e79e7b2f0c8f3de24588
Nikita Goncharov <nikitagon5@gmail.com>
2025-01-10 13:56:43 -0500

    tweaking pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,19 @@
 module.exports = {
     apps: [{
-        name: 'codeapp',
+        name: 'scriptorium-prod',
         script: 'npm',
         args: 'start',
+        cwd: '/home/nickin/Desktop/scriptorium',
         env: {
             NODE_ENV: 'production',
+            PORT: 3000
         },
-        watch: false,
+        instances: 6,
+        exec_mode: 'cluster',
+        max_memory_restart: '300M',
         error_file: 'logs/error.log',
         out_file: 'logs/output.log',
         log_date_format: 'YYYY-MM-DD HH:mm:ss',
-        merge_logs: true,
-        instances: '6',
-        exec_mode: 'cluster',
-        max_memory_restart: '300M'  // Restart if memory exceeds this
+        merge_logs: true
     }]
 } 
